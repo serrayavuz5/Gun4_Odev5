c5caf679cbe8892b766c0283c5b6beb0694d7876
Serra Yavuz <srryavuz@gmail.com>
2021-01-31 04:44:31 +0300

    Proje dosyası ekle.

---
+++ Odev5/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev5
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        //tepmArray = geçici dizi
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ Odev5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Serra", "Yavuz");
+            myDictionary.Add("Engin", "Demiroğ");
+            Console.WriteLine(myDictionary.Keys[0] + " " + myDictionary.Values[0]);
+            Console.WriteLine(myDictionary.Keys[1] + " " + myDictionary.Values[1]);
+        }
+    }
+}
